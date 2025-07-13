9ea8e69628757bbd908dcb7c12b3ca94ecbcb1de
zMaliu <znaihe@hotmail.com>
2025-07-13 17:35:19 +0800

    maliu

--- F_sun.m
+++ F_sun.m
@@ -1,33 +1,33 @@
 function [x,y,z]=F_sun(month,hour,minute)
-    D_list=[306,337,0,31,61,92,122,153,184,214,245,275];
+    D_list= [21, 52, 80, 111, 141, 172, 202, 233, 264, 294, 325, 355];
     D=D_list(month);
     ST=hour+minute/60;
     w=(ST-12)*15;
-    weidu=39.4;
+    phi=39.4;
 
-    %赤纬角delta
-    delta = 23.45*sin(2*pi*(D+284)/365);
+    %赤纬角deta
+    deta = 23.45*sin(2*pi*(D+284)/365);
 
     %计算高度角
-    weidu_rad = deg2rad(weidu);
-    delta_rad = deg2rad(delta);
+    phi_rad = deg2rad(phi);
+    delta_rad = deg2rad(deta);
     w_rad = deg2rad(w);
-    sin_alpha = cos(delta_rad) * cos(weidu_rad) * cos(w_rad) + sin(delta_rad) * sin(weidu_rad);
+    sin_alpha = cos(delta_rad) * cos(phi_rad) * cos(w_rad) + sin(delta_rad) * sin(phi_rad);
     sin_alpha = max(min(sin_alpha, 1), -1); 
-    alpha_s_rad = asin(sin_alpha); 
-    cos_alpha = cos(alpha_s_rad); % 高度角余弦值
+    a_s = asin(sin_alpha); 
+    cos_alpha = cos(a_s); % 高度角余弦值
 
 
     %计算方位角
-    cos_Y = (sin(delta_rad) - sin(alpha_s_rad) * sin(phi_rad)) / (cos_alpha * cos(phi_rad));
+    cos_Y = (sin(delta_rad) - sin(a_s) * sin(phi_rad)) / (cos_alpha * cos(phi_rad));
     cos_Y = max(min(cos_Y, 1), -1); 
-    Y_s_rad = acos(cos_Y); 
+    y_s = acos(cos_Y); 
     % 上午（时角w<0）方位角为负，下午（w>0）为正
     if w < 0
-        Y_s_rad = -Y_s_rad;
+        y_s = -y_s;
     end
-    x = sin(Y_s_rad) * cos_alpha; 
-    y = cos(Y_s_rad) * cos_alpha; 
+    x = sin(y_s) * cos_alpha; 
+    y = cos(y_s) * cos_alpha; 
     z = sin_alpha;
 
     x = round(x, 4);
