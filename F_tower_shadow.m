c0ca37f98f6a3763e1a36f529c6e40d2d592f796
zMaliu <znaihe@hotmail.com>
2025-07-15 20:56:40 +0800

    塔遮挡阴影（未完整）

--- F_tower_shadow.m
+++ F_tower_shadow.m
@@ -3,6 +3,7 @@ function tower_shadow=F_tower_shadow(Mirror,Tower,a_s,y_s)
     if a_s<1e-6
         tower_shadow=1;
         return;
+    end
     %塔高、塔长与太阳半张角
     H_tower=Tower.z;
     R_tower=Tower.R;
@@ -10,7 +11,7 @@ function tower_shadow=F_tower_shadow(Mirror,Tower,a_s,y_s)
 
     n_shadow=[-cos(a_s)*sin(y_s),-cos(a_s)*cos(y_s)];%阴影方向向量
     L_shadow=H_tower/tan(a_s);%阴影长度
-    Center_shadow=[tower.x,tower.y]+n_shadow*L_shadow%阴影中心坐标
+    Center_shadow=[tower.x,tower.y]+n_shadow*L_shadow;%阴影中心坐标
     R_shadow=R_tower+H_tower*tan(sigma)/tan(a_s);%阴影半径
 
     d=norm([Mirror.x-Center_shadow(1),Mirror.y-Center_shadow(2)]);%定日镜与阴影的距离
@@ -23,4 +24,5 @@ function tower_shadow=F_tower_shadow(Mirror,Tower,a_s,y_s)
     else%计算阴影面积占比
         area=
         tower_shadow=area/(Mirror.w*Mirror.h);
+    end
 end
