c6b40abecfc9107b45f287eb70c06588ee145f18
zMaliu <znaihe@hotmail.com>
2025-07-16 21:58:49 +0800

    maliu

---
+++ F_36points.m
@@ -0,0 +1,12 @@
+
+function point = F_36points(x,y,a_m,y_m)
+    point=zeros(6,6,3);
+    pd=[x,y,4];
+
+    for i=1:1:6
+        for j=1:1:6
+            pm = [-4+i,-4+j,0];
+            point(i,j) = F_ma_to_d(pd,a_m,y_m,pm);
+        end
+    end
+end
