52b6019b7be00bb394966b31ca7a9830fb7f07b7
kuixin1 <2229697964@qq.com>
2025-07-18 15:52:06 +0800

    1

--- F_IOline.m
+++ F_IOline.m
@@ -1,3 +1,8 @@
-function [n_inline,n_outline]=F_IOline()
-    
+% 假定太阳距离原点500m 这样才可以假定太阳的坐标
+function [n_inline,n_outline]=F_IOline(p,a_s,y_s)
+    l = 500;
+    % x y 坐标可以为负数，但是 z 为正数
+    I_x=-sin(y_s_rad)*cos(a_s_rad);
+    I_y=-cos(y_s_rad)*cos(a_s_rad);
+    I_z=-sin(a_s_rad);
 end
--- F_change_toMirror.m
+++ F_change_toMirror.m
@@ -5,8 +5,8 @@ function pm = F_change_toMirror(a_m,y_m,pd,p)
     T_b = [cos(y_m),-sin(y_m),0;
             sin(y_m),cos(y_m),0;
             0,0,1];
-    T_a_inv = T_a'; % 由于 T_a 是正交矩阵，其逆等于转置
-    T_b_inv = T_b'; % 由于 T_b 是正交矩阵，其逆等于转置
-    T = T_b_inv * T_a_inv; % 利用 (AB)^-1 = B^-1 A^-1
+    T_a_inv = T_a'; 
+    T_b_inv = T_b'; 
+    T = T_b_inv * T_a_inv; 
     pm = (p - pd) * T;
 end
