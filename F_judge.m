db42a606146b5e3b2472197c2dea71a1a04a93ad
kuixin1 <2229697964@qq.com>
2025-07-21 15:49:59 +0800

    函数

--- F_judge.m
+++ F_judge.m
@@ -1,21 +1,42 @@
-function bool = F_judge(point_inMirrorA,out_line,point_MirrorB,n_b)
+function bool = F_judge(point_inMirrorA,n_inline,n_out_line,point_MirrorB,n_b)
     w = 3;
+    % 入射的判断
+    % 从a指向太阳
+    d1 = -n_inline;
+    l1 = dot(n_b, point_MirrorB - point_onMirrorA);     % 几何意义：向量 (b - a) 在法向量方向上的投影长度
+    theta = dot(n_b, d);       % 几何意义：直线方向 d 与法向量 n_b 的夹角余弦
+    t1 = l1 / theta;     % 几何意义：从 a 出发，沿方向 d 走多远能碰到平面
+    p1 = point_onMirrorA + t1 * d1;    % 从点 a 出发，沿着方向 d 走了长度 t 后
+    e11 = cross([0 0 1], n_b);
+    if norm(e11) < 1e-8
+        e11 = [1 0 0];
+    else
+        e11 = e11 / norm(e11);   % 单位化
+    end
+    e21 = cross(n_b, e11);
+    % 交点在 以镜面中心为原点、以 e1、e2 为轴的二维坐标系 中的坐标
+    local = [dot(p1 - point_MirrorB, e11), dot(p1 - point_MirrorB, e21)];
+    % 判断是否在矩阵内
+    bool1 = abs(local(1)) <= w && abs(local(2)) <= w;
+
+
+    % 反射的判断
     r = point_MirrorB - point_inMirrorA;
 
     l = dot(n_b, r);
     if abs(l) < 1e-8          % 平行或共面
-        bool = false;
+        bool2 = false;
         return;
     end
 
-    t = l / dot(n_b, out_line) ;
+    t = l / dot(n_b, n_out_line) ;
     if t < 0                       % 反向无意义
-        bool = false;
+        bool2 = false;
         return;
     end
 
     % 交点坐标
-    p = point_inMirrorA + t * out_line;
+    p = point_inMirrorA + t * n_out_line;
 
     % 判断交点在不在定日镜上
     % 建立局部坐标系
@@ -29,5 +50,7 @@ function bool = F_judge(point_inMirrorA,out_line,point_MirrorB,n_b)
     % 交点在 以镜面中心为原点、以 e1、e2 为轴的二维坐标系 中的坐标
     local = [dot(p - point_MirrorB, e1), dot(p - point_MirrorB, e2)];
     % 判断是否在矩阵内
-    bool = abs(local(1)) <= w && abs(local(2)) <= w;
+    bool2 = abs(local(1)) <= w && abs(local(2)) <= w;
+
+    bool = bool1 && bool2;
 end
--- F_n_trunc.m
+++ F_n_trunc.m
@@ -1,4 +1,45 @@
 % 射出去到达集热器的点/每个镜子实际射出去的点
-function n_trunc = F_n_trunc(points)
-    
+function n_trunc = F_n_trunc(points,a_s,y_s,n_m)
+    n = size(points,1);
+    r = 3.5;
+    zmin = 80;
+    zmax = 88;
+    ta_n = 0;
+    % 遍历每个点，得到每个点的反射光线
+    for i=1:1:n
+        p = points(i,:);
+        [~,n_outline]=F_IOline(p,a_s,y_s,n_m);
+        x = p(1); y = p(2); z = p(3);
+        dx = n_outline(1); dy = n_outline(2); dz = n_outline(3);
+
+        A = dx^2 + dy^2;
+        B = 2*(x*dx + y*dy);
+        C = x^2 + y^2 - r^2;
+        % 计算判别式
+        delta = B^2 - 4*A*C;
+        % 求出两个根
+        sqrtDelta = sqrt(delta);
+        t1 = (-B - sqrtDelta) / (2*A);
+        t2 = (-B + sqrtDelta) / (2*A);
+
+        % 取最小的非负根
+        t = inf;
+        if t1 >= 0
+            t = t1;
+        elseif t2 >= 0
+            t = t2;
+        else
+            hitFlag = false;
+            return;
+        end
+
+        % 查看高度是否在范围内
+        z_hit = z + t*dz;
+        hitFlag = (z_hit >= zmin) && (z_hit <= zmax);
+        if hitFlag()
+            ta_n = ta_n + 1;
+        end
+
+    end
+    n_trunc = ta_n / n;
 end
