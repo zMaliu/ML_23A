6da38ad43f5cce05689725a5aeb120f1b2324262
zMaliu <znaihe@hotmail.com>
2025-07-24 11:19:06 +0800

    ml

--- S_q1.m
+++ S_q1.m
@@ -20,6 +20,10 @@ for month = months
 end
 
 arrry_n_light=zeros(1,60);
+array_n_sb=[];
+array_n_trunc=[];
+array_n_cos=[];
+array_n_at=[];
 n_ref=0.92;
 %part1：计算每个定日镜的阴影遮挡效率n_sb  以数组array_n_sb输出
 for i=1:length(Momments)%遍历时刻
@@ -43,12 +47,13 @@ for i=1:length(Momments)%遍历时刻
     %1.2、遍历Mirrors每个定日镜 进行阴影遮挡效率计算
     for j=1:length(new_Mirrors)%每个定日镜
         mirror_site=Mirrors{j};
-        mirror_site=[mirror_site];
         tower_site=[0,0,80];
 
         %part3：计算大气透射率 以数组array_n_at输出
         n_cos=F_n_cos(a_s,y_s,mirror_site,tower_site);
         n_at=F_n_at(mirror_site,tower_site);
+        array_n_at=[array_n_at,n_at];
+        array_n_cos=[array_n_cos,n_cos];
 
         %F_potential_tower:输入Mirrors数组 输出距离Mirrors(j)长度小于R的所有定日镜 即potential_Mirrors
         potential_Mirrors=F_potential_tower(mirror_site,new_Mirrors,r);
@@ -58,10 +63,9 @@ for i=1:length(Momments)%遍历时刻
         list_36points=F_36points(mirror_site,a_m,y_m);
         % 没有被遮挡到的点的集合 new_points
         new_points={};
+        res=0;
         for k=1:36%遍历36个点
             pointsite_36=list_36points{k};
-            res=0;
-            
             %F_IOline:输入点坐标、太阳方位角、高度角 输出入射/反射光线方向向量
             [n_inline,n_outline]=F_IOline(pointsite_36,a_s,y_s,n);
             % 判断点是否被遮挡
@@ -71,20 +75,23 @@ for i=1:length(Momments)%遍历时刻
                 [n_Mb,a_m,y_m]=F_mirror(a_s,y_s,point_inMb,tower_site);
                 %F_judge:输入点坐标 入射/反射向量 定日镜坐标、法向量 输出true/false(是否相交)
                 if F_judge(pointsite_36,n_inline,n_outline,point_inMb,n_Mb)
-                    res=res+1;
                     sign = false;
 
                 end
             end
             if sign
                 new_points{end + 1} = pointsite_36;
+                res=res+1;
             end
         end
         n_sb=res/36;%单个定日镜的遮挡效率
+        array_n_sb=[array_n_sb,n_sb];
 
         %part4：计算截断效率 以数组array_n_trunc输出
         total_points_in36=length(new_points);
         n_trunc=F_n_trunc(new_points,a_s,y_s,n);
+        array_n_trunc=[array_n_trunc,n_trunc];
+
         n_light=n_trunc*n_sb*n_at*n_cos*n_ref;
         total_n_light=total_n_light+n_light;
     end
